d867e716e4ea61bc2acb7b897fd9c7dbd7be30f5
ProDataMan <antoine.victor@ProDataMan.com>
2024-07-03 14:59:29 -0700

    test

--- test_firefox.py
+++ test_firefox.py
@@ -1,23 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-import time
-import logging
 
-# Set up logging for debugging
-from selenium.webdriver.remote.remote_connection import LOGGER
-LOGGER.setLevel(logging.DEBUG)
-
-# Set up Firefox options
 options = Options()
 options.headless = True
 
-# Define Firefox service with explicit Marionette port and log path
-service = Service(log_path='/tmp/geckodriver.log', service_args=['--marionette-port', '2828'])
-
 try:
-    # Initialize the Firefox driver with the specified service
-    driver = webdriver.Firefox(service=service, options=options)
+    driver = webdriver.Firefox(options=options)
     driver.get("http://www.google.com")
     print(driver.title)
     driver.quit()

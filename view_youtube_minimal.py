c5888bb04157772a633f3f9b50c543acb9628722
ProDataMan <antoine.victor@prodataman.com>
2024-07-24 18:53:23 -0700

    new video link and 7 minute timer

--- view_youtube_minimal.py
+++ view_youtube_minimal.py
@@ -25,7 +25,7 @@ try:
     logging.info("Chromium driver initialized successfully.")
 
     # Navigate to the YouTube livestream URL
-    livestream_url = "https://youtu.be/y9teDYPTeok"
+    livestream_url = "https://youtu.be/TnAKwEiFOxM"
     logging.info(f"Navigating to YouTube livestream: {livestream_url}")
     driver.get(livestream_url)
     logging.info("Navigated to YouTube livestream.")
@@ -51,7 +51,7 @@ try:
         logging.info(f"No play button found: {e}")
 
     # Wait to ensure the view is registered
-    wait_time = 25 * 60  # 2 minutes
+    wait_time = 7 * 60  # 2 minutes
     logging.info(f"Waiting for {wait_time} seconds to ensure view is registered...")
     time.sleep(wait_time)
     logging.info("Waited for 2 minutes.")
